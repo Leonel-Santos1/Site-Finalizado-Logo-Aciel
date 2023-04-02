c3cf1c4c003ab21772782aad95b9340cc4afa266
Leonel <franciscoleonelfds@gmail.com>
2023-04-02 00:34:06 -0300

    quebra cabeça

--- script.js
+++ script.js
@@ -11,35 +11,55 @@ image.addEventListener("load", function () {
 
 
   NameInputs.addEventListener("keyup", (keyup) => {
-    let Pength = Canvas.measureText(NameInputs.value + "a").width
-    Canvas.fillStyle = "blue";
-    Canvas.fillRect(2, 20, Pength, 70);
 
-    
-    Canvas.fillStyle = "black";
-    Canvas.font = "48px serif";
-
-    Canvas.fillText(" "+ NameInputs.value + "", 0, 70);
+    WriteTextCanvas(NameInputs);
 
     Canvas.clearRect(0, 0, 530, 420);
     Canvas.drawImage(image, 0, 0, 530, 420);
 
-    Pength = Canvas.measureText(NameInputs.value + "a").width
-    Canvas.fillStyle = "blue";
-    Canvas.fillRect(2, 20, Pength, 70);
+    WriteTextCanvas(NameInputs);
 
-    
-    Canvas.fillStyle = "black";
-    Canvas.font = "48px serif";
-
-    Canvas.fillText(" "+ NameInputs.value + "", 0, 70);
   });
 });
+
 image.src = "lulaNao.png";
 
-// https://cristiantela.github.io/lula2turno/base.png
+function WriteTextCanvas(NameInputs) {
+  const Pength = Canvas.measureText(NameInputs.value + "a").width
+  Canvas.fillStyle = "blue";
 
-// document.getElementById("teste").innerText = NameInputs.value
-// console.log("Valor ao ser pressionado pelo backspace", document.getElementById("teste"))
+  // Define o tamanho do retângulo
+  const rectHeight = 100;
 
-// document.getElementById("teste").innerText = Key
+  // Define o raio dos cantos arredondados
+  const cornerRadius = 20;
+
+  // Define a posição do retângulo
+  const rectX = Canvas.width / 2 - Pength / 2;
+  const rectY = Canvas.height / 2 - rectHeight / 2;
+
+  // Desenha o retângulo com cantos arredondados
+  Canvas.beginPath();
+  Canvas.lineTo(rectX + Pength - cornerRadius, rectY);
+  Canvas.arcTo(rectX + Pength, rectY, rectX + Pength, rectY + cornerRadius, cornerRadius);
+  Canvas.lineTo(rectX + Pength, rectY + rectHeight - cornerRadius);
+  Canvas.arcTo(rectX + Pength, rectY + rectHeight, rectX + Pength - cornerRadius, rectY + rectHeight, cornerRadius);
+  Canvas.lineTo(rectX + cornerRadius, rectY + rectHeight);
+  Canvas.arcTo(rectX, rectY + rectHeight, rectX, rectY + rectHeight - cornerRadius, cornerRadius);
+  Canvas.lineTo(rectX, rectY + cornerRadius);
+  Canvas.arcTo(rectX, rectY, rectX + cornerRadius, rectY, cornerRadius);
+  Canvas.closePath();
+  Canvas.fillStyle = 'blue';
+  Canvas.fill();
+  Canvas.fillRect(2, 20, Pength, 70);
+
+
+  Canvas.fillStyle = "black";
+  Canvas.font = "48px serif";
+
+  Canvas.fillText(" " + NameInputs.value + "", 0, 70);
+
+  
+
+
+}
